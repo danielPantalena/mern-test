26e2c62eb82aff410ce3bf6fdadc4c7a7a4292b0
Daniel Pantalena <pantalenadaniel@gmail.com>
2021-01-23 13:18:07 -0300

    Rename to adminsController

---
+++ src/controllers/adminsController.ts
@@ -0,0 +1,25 @@
+import { Router, Request, Response } from 'express';
+import { validateAccount } from '../middlewares/validateAccount';
+import { generateErrorJSON } from '../functions';
+import {
+  create,
+  readAll,
+  readOneById,
+  readOneByName,
+  updateById,
+  deleteById,
+} from '../models/accountsModel';
+
+const adminsController = Router();
+
+adminsController.post('/', validateAccount, async ({ body }: Request, res: Response) => {
+  try {
+    const createdAccount = await create(body);
+    return res.status(201).json(createdAccount);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json(generateErrorJSON(error.message));
+  }
+});
+
+export default adminsController;
