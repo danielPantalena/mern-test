8031c56eb53708ccbf0fb84bf5c914cdd268b823
Daniel Pantalena <pantalenadaniel@gmail.com>
2021-01-23 13:17:03 -0300

    Integrate usersController

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,8 +1,8 @@
 export const generateErrorJSON = (messageParam: string) => {
   const message = messageParam ? messageParam : 'Opss... something went wrong';
-  ({
+  return {
     error: true,
     ok: false,
     message,
-  });
+  };
 };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import * as dotenv from 'dotenv';
 import accountsController from './controllers/accountsController';
+import usersController from './controllers/usersController';
 
 dotenv.config();
 
@@ -11,5 +12,6 @@ const app = express();
 app.use(express.json());
 
 app.use('/admin', accountsController);
+app.use('/', usersController);
 
 app.listen(PORT, () => console.log(`Listening at PORT:${PORT}`));
