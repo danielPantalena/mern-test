bbce553914457aa93d60dd578dd3e6447d855409
Daniel Pantalena <pantalenadaniel@gmail.com>
2021-01-23 17:47:03 -0300

    Use cors

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT ?? 3000;
 const app = express();
 
 app.use(express.json());
-// app.use(cors())
+app.use(cors())
 
 app.use('/admin', adminsController);
 app.use('/', usersController);
