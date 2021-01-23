a09a29fe3dc91320eb40f8e444944b60e1035010
Daniel Pantalena <pantalenadaniel@gmail.com>
2021-01-23 10:58:50 -0300

    Type express

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
-const express = require('express')
-require('dotenv/config')
+import * as express from 'express'
+import { Request, Response } from 'express'
+import * as dotenv from 'dotenv';
 
+dotenv.config()
 
 const PORT = process.env.PORT ?? 3000
 
 const app = express();
 
-app.get('/', (_req, res) => { res.send('Hello World!') })
+app.get('/', (_req: Request, res: Response) => { res.send('Hi') })
 
 
 app.listen(PORT, () => console.log(`Listening at PORT:${PORT}`))
