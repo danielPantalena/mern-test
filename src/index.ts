b90b28bfa5d8d10a8d34be0962909334df7ae544
Daniel Pantalena <pantalenadaniel@gmail.com>
2021-01-24 18:52:20 -0300

    Create pushNotifications controller

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import * as express from 'express';
 import * as dotenv from 'dotenv';
 import * as cors from 'cors';
-import adminsController from './controllers/adminsController';
-import usersController from './controllers/usersController';
+import { adminsController, usersController, pushNotificationsController } from './controllers';
 
 dotenv.config();
 
@@ -11,9 +10,10 @@ const PORT = process.env.PORT ?? 3000;
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.use('/admin', adminsController);
+app.use('/notification', pushNotificationsController);
 app.use('/', usersController);
 
 app.listen(PORT, () => console.log(`Listening at PORT:${PORT}`));
